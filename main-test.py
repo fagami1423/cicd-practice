97ee61d96afa8b1e98f046964557a8fc80d43697
Raj Kumar Phagami <magar.fagami@gmail.com>
2023-02-07 13:51:38 -0500

    debugged

--- main-test.py
+++ main-test.py
@@ -1,9 +1,9 @@
 # Import the Add function, and assert that it works correctly.
-from main import Add
+from main import add
 
 def TestAdd():
-        assert Add(2,3) == 5
-        assert Add(5,5) == 10
+        assert add(2,3) == 5
+        assert add(5,5) == 10
         print("Add Function works correctly")
 
 if __name__ == '__main__':
